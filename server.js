2038d44781d20457e180cbce0a9d55f417f37624
Joseph-kaku <samkaks18@gmail.com>
2023-10-12 14:49:58 -0600

    having a cors issue

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const dotenv = require('dotenv')
 dotenv.config()
 const bodyParser = require('body-parser');
 const mongodb = require("./db/connect");
+const cors = require('cors')
 
 mongodb.initDb(Error);
 
 const port = process.env.PORT || 8080
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/', require('./routes/route'));
 
 app.listen(port, () => {
-    console.log(`Server is running on Port http://localhost:${port}`)
+    console.log(`Server is running on Port http://localhost:${port}/shoes`)
 });
