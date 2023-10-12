7bd3a1d18c2b2f88331f964b1234c15011743a89
Joseph-kaku <samkaks18@gmail.com>
2023-10-12 15:48:31 -0600

    this is driving me insane

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = express();
 const dotenv = require('dotenv')
 dotenv.config()
 const bodyParser = require('body-parser');
+// const cors = require('cors');
 const mongodb = require("./db/connect");
-const cors = require('cors')
 
 mongodb.initDb(Error);
 
 const port = process.env.PORT || 8080
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use('/', require('./routes/route'));
 
 app.listen(port, () => {
-    console.log(`Server is running on Port http://localhost:${port}/shoes`)
+    console.log(`Server is running on Port http://localhost:${port}`)
 });
