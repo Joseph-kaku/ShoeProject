0ff50d0eaa31b72a3f56ab8d5f4a74ba520da878
Joseph-kaku <samkaks18@gmail.com>
2023-10-12 18:17:44 -0600

    problem fixed

--- swagger.js
+++ swagger.js
@@ -5,15 +5,12 @@ info: {
     title: 'My API',
     description: 'Description',
 },
-host: ('localhost:3000'),
+host: ('localhost:8080'),
 schemes: ['http'],
 };
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./routes/route'];
 
-/* NOTE: if you use the express Router, you must pass in the 
-'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
