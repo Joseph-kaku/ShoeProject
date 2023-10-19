1b1c0e1cb7d5144889ba83864b3372601223c023
Joseph-kaku <samkaks18@gmail.com>
2023-10-18 18:15:04 -0600

    added an error check

--- routes/owners.js
+++ routes/owners.js
@@ -3,7 +3,10 @@ const controllers = require('../controllers/owner')
 const regValidate = require('../utilities/validate')
 
 routes.get('/', controllers.getAllOwners)
-routes.post('/', regValidate.addNewOwnerRules(), controllers.addNewOwner)
+routes.post('/', 
+regValidate.addNewOwnerRules(), 
+regValidate.checkOwnerData,
+controllers.addNewOwner)
 routes.get('/:id', controllers.getSingleOwner)
 routes.put('/:id', controllers.updateOwner)
 routes.delete('/:id', controllers.deleteOwner)
--- routes/shoes.js
+++ routes/shoes.js
@@ -3,7 +3,10 @@ const controllers = require('../controllers/shoes')
 const regValidate = require('../utilities/validate')
 
 routes.get('/', controllers.getAllShoes)
-routes.post('/', regValidate.addNewShoeRules(), controllers.addNewShoe);
+routes.post('/', 
+regValidate.addNewShoeRules(),
+regValidate.checkShoeData,
+controllers.addNewShoe);
 routes.get('/:id', controllers.getSingleShoe);
 routes.put('/:id', controllers.updateShoe)
 routes.delete('/:id', controllers.deleteShoe)
