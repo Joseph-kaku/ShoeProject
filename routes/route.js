dce0d45f9a8b6c0bd43a5f1d6fdd60624a6f334f
Joseph-kaku <samkaks18@gmail.com>
2023-10-23 18:19:48 -0600

    Oauth added

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,36 @@
 const routes = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
+const { auth } = require('express-openid-connect');
+
+const { requiresAuth } = require('express-openid-connect');
 
 routes.use('/api-docs', swaggerUi.serve);
 routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'a long, randomly-generated string stored in env',
+    baseURL: 'http://localhost:8080',
+    clientID: '69NCwtlBngsYRciHfCYTooXXrkEeTsl3',
+    issuerBaseURL: 'https://dev-p3emw77r66837ncj.us.auth0.com'
+    };
+    
+    // auth router attaches /login, /logout, and /callback routes to the baseURL
+    routes.use(auth(config));
+    
+    // req.isAuthenticated is provided from the auth router
+    routes.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    });
+
+routes.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+
 // routes.use('/', require())
-routes.use('/shoes', require('./shoes'))
-routes.use('/owners', require('./owners'))
+routes.use('/shoes', requiresAuth(), require('./shoes'))
+routes.use('/owners',requiresAuth(), require('./owners'))
 
 module.exports = routes
